16be366a887e3b0322b8dd0c350a570fa0424a81
Emir Vardar <emir.vardar38@gmail.com>
2024-12-04 11:38:49 +0300

    Proje dosyası ekle.

---
+++ bayBayanKuafor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bayBayanKuafor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
